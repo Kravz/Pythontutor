a54b721cacbf91262b45eb9581be40980cf142c7
Kravz <41003193+Kravz@users.noreply.github.com>
2018-07-09 16:47:44 +0300

    Add files via upload

---
+++ 2./Знак числа.py
@@ -0,0 +1,16 @@
+x = int(input())
+if x > 0:
+    x = 1
+elif x < 0:
+    x = -1
+else:
+    x= 0
+print(x)
+"""
+if x > 0:
+print(1)
+elif x < 0:
+print(-1)
+else:
+print(0)
+"""
---
+++ 2./Минимум из двух чисел.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+if x > 0 and x < y:
+    print(x)
+elif x < y:
+    print(x)
+else:
+    print(y)
